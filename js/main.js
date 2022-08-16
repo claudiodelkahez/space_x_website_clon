5e38d9b24739c38d5b56e6fc3754e6b73bffb10a
Claudio Delkahez <cdelkahez.dev@gmail.com>
2022-08-15 23:35:14 -0300

    Dark overlay & remove on scroll

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 const btn = document.querySelector('.hamburger');
+const overlay = document.querySelector('.overlay')
+
+document.addEventListener('scroll',  removeOverlay);
+
+function removeOverlay() {
+    overlay.classList.remove('show')
+}
 
 btn.addEventListener('click', hamburgerToggle);
 
 function hamburgerToggle() {
     btn.classList.toggle('open')
+    overlay.classList.toggle('show')
 }
