4cc3a93aff56a7f604ef11fb7dc615c5f17aef48
Claudio Delkahez <cdelkahez.dev@gmail.com>
2022-08-15 23:57:23 -0300

    Side menu html & css

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 const btn = document.querySelector('.hamburger');
 const overlay = document.querySelector('.overlay')
-
+const menu = document.querySelector('.mobile-main-menu')
 document.addEventListener('scroll',  removeOverlay);
 
 function removeOverlay() {
     overlay.classList.remove('show')
+    menu.classList.remove('show-menu');
 }
 
 btn.addEventListener('click', hamburgerToggle);
 
 function hamburgerToggle() {
-    btn.classList.toggle('open')
-    overlay.classList.toggle('show')
+    btn.classList.toggle('open');
+    overlay.classList.toggle('show');
+    menu.classList.toggle('show-menu');
 }
